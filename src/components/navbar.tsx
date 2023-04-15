f70c717d9878d57afaf64ce72240031e1e2f1097
Zisis Kostakakis <zisis.kostakakis@stormharvester.com>
2023-04-15 23:21:06 +0300

    small test fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import { UserButton } from '@clerk/nextjs/app-beta';
 import React from 'react';
 
@@ -14,14 +13,12 @@ const Navbar: React.FC<navbarProps> = ({}) => {
             Transport-Info
           </span>
         </a>
-        <button
+        {/* <button
           data-collapse-toggle="navbar-default"
           type="button"
-          className="hover:hover:bg-gray-700focus:outline-none
-           ml-3 inline-flex items-center rounded-lg p-2
-             text-sm text-gray-400 focus:ring-2 focus:ring-gray-600  md:hidden"
-          aria-controls="navbar-default"
+          className="ml-3 inline-flex items-center rounded-lg p-2 text-sm text-gray-400 hover:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600  md:hidden"
           aria-expanded="false"
+          aria-controls="navbar-default"
         >
           <span className="sr-only">Open main menu</span>
           <svg
@@ -30,15 +27,17 @@ const Navbar: React.FC<navbarProps> = ({}) => {
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
+            area-aria-expanded="True"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
-        </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        </button> */}
+
+        <div className=" w-full md:block md:w-auto">
           <ul
             className="mt-4 flex flex-col rounded-lg border border-gray-700 bg-gray-800 p-4 
           font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-gray-900 md:p-0"
